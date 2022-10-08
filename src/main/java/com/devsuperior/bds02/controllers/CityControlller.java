1dc1ead655926972f2b20daede10d900161c05c4
Otávio Landim <otavio.landim@gmail.com>
2022-10-07 21:10:24 -0300

    Adding new controller

---
+++ src/main/java/com/devsuperior/bds02/controllers/CityControlller.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds02.controllers;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityControlller {
+
+    @Autowired
+    private CityService service;
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
